efc42dd7d049cd54211ada764b310d2cebbc0cfe
danielgongora0204 <43617580+danielgongora0204@users.noreply.github.com>
2022-02-07 00:04:44 -0800

    Update Login, added logo

---
+++ app/src/main/java/com/example/hue/views/activities/LoginActivity.kt
@@ -0,0 +1,10 @@
+package com.example.hue.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/hue/views/activities/SplashActivity.kt
+++ app/src/main/java/com/example/hue/views/activities/SplashActivity.kt
@@ -1,15 +1,10 @@
-package com.example.hue
+package com.example.hue.views.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
-
-
-
-
 }

ecaf6367176cc69ef97be6ff345f346e3f03541c
danielgongora0204 <43617580+danielgongora0204@users.noreply.github.com>
2022-06-03 16:18:07 -0700

    Updated DataStore implementation

--- app/src/main/java/com/gig/hue/data/DataStoreManager.kt
+++ app/src/main/java/com/gig/hue/data/DataStoreManager.kt
@@ -2,56 +2,51 @@ package com.gig.hue.data
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.preferencesKey
-import androidx.datastore.preferences.createDataStore
+import androidx.datastore.preferences.core.*
+import androidx.datastore.preferences.preferencesDataStore
 import com.gig.hue.helper.constants.DataStoreConstants
-import kotlinx.coroutines.flow.Flow
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
+import java.io.IOException
+import javax.inject.Inject
 
-class DataStoreManager(context: Context) {
-    private val dataStore: DataStore<Preferences> = context.createDataStore(DataStoreConstants.DATA_STORE_NAME)
+private val Context.dataStore by preferencesDataStore(DataStoreConstants.DATA_STORE_NAME)
 
-    fun readBoolean(key: Preferences.Key<Boolean>): Flow<Boolean?> {
-        return dataStore.data.map { preferences ->
-            preferences[key]
+class DataStoreManager @Inject constructor(@ApplicationContext context: Context) {
+
+    private val dataStore = context.dataStore
+
+    private suspend fun <T> DataStore<Preferences>.getFromLocalStorage(
+        PreferencesKey: Preferences.Key<T>, func: T.() -> Unit) {
+        data.catch {
+            if (it is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw it
+            }
+        }.map {
+            it[PreferencesKey]
+        }.collect {
+            it?.let { func.invoke(it as T) }
         }
     }
 
-    fun readString(key: Preferences.Key<String>): Flow<String?> {
-        return dataStore.data.map { preferences ->
-            preferences[key]
+    suspend fun <T> storeValue(key: Preferences.Key<T>, value: T) {
+        dataStore.edit {
+            it[key] = value
         }
     }
 
-    fun readInt(key: Preferences.Key<Int>): Flow<Int?> {
-        return dataStore.data.map { preferences ->
-            preferences[key]
-        }
-    }
-
-    suspend fun writeString(key: Preferences.Key<String>, value: String) {
-        dataStore.edit { settings ->
-            settings[key] = value
-        }
-    }
-
-    suspend fun writeBoolean(key: Preferences.Key<Boolean>, value: Boolean) {
-        dataStore.edit { settings ->
-            settings[key] = value
-        }
-    }
-
-    suspend fun writeInt(key: Preferences.Key<Int>, value: Int) {
-        dataStore.edit { settings ->
-            settings[key] = value
+    suspend fun <T> readValue(key: Preferences.Key<T>, responseFunc: T.() -> Unit) {
+        dataStore.getFromLocalStorage(key) {
+            responseFunc.invoke(this)
         }
     }
 
     companion object PreferencesKeys {
-        val REMEMBER_ME = preferencesKey<Boolean>(DataStoreConstants.DATA_STORE_LOGIN_REMEMBER_ME)
-        val LOGIN_USERNAME = preferencesKey<String>(DataStoreConstants.DATA_STORE_LOGIN_USERNAME)
-        val LOGIN_PASSWORD = preferencesKey<String>(DataStoreConstants.DATA_STORE_LOGIN_PASSWORD)
+        val REMEMBER_ME = booleanPreferencesKey(DataStoreConstants.DATA_STORE_LOGIN_REMEMBER_ME)
+        val LOGIN_USERNAME = stringPreferencesKey(DataStoreConstants.DATA_STORE_LOGIN_USERNAME)
+        val LOGIN_PASSWORD = stringPreferencesKey(DataStoreConstants.DATA_STORE_LOGIN_PASSWORD)
     }
 }

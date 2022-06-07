236d69a42259e05c799d51d75d702070d014228c
danielgongora0204 <43617580+danielgongora0204@users.noreply.github.com>
2022-06-06 19:39:18 -0700

    Update Converters.kt

--- app/src/main/java/com/gig/hue/data/Converters.kt
+++ app/src/main/java/com/gig/hue/data/Converters.kt
@@ -6,13 +6,10 @@ import java.time.LocalDateTime
 class Converters {
 
     @TypeConverter
-    fun fromString(value: String?): LocalDateTime? {
-        return value?.let{ LocalDateTime.parse(value) }
-    }
+    fun fromString(value: String?): LocalDateTime? = value?.let{ LocalDateTime.parse(value) }
 
     @TypeConverter
-    fun fromDateToString(value: LocalDateTime?): String? {
-        return value?.toString()
-    }
+    fun fromDateToString(value: LocalDateTime?): String? = value?.toString()
+
 
 }

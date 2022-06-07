f7628522984a243c3d6e8bce1ba6551d68ee2b5f
danielgongora0204 <43617580+danielgongora0204@users.noreply.github.com>
2022-06-06 20:20:41 -0700

    Updated View Model
    Implemented Is User Logged In

--- app/src/main/java/com/gig/hue/view_models/SplashViewModel.kt
+++ app/src/main/java/com/gig/hue/view_models/SplashViewModel.kt
@@ -1,18 +1,18 @@
 package com.gig.hue.view_models
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.gig.hue.com.gig.hue.data.repositories.SplashRepository
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 @HiltViewModel
-class SplashViewModel @Inject constructor(@ApplicationContext val context: Context) : ViewModel() {
+class SplashViewModel @Inject constructor(private val splashRepository: SplashRepository) : ViewModel() {
     //region Variables
     private var auth = Firebase.auth;
-
+    val isUserLoggedIn = splashRepository.isUserLoggedIn().asLiveData()
     //endregion
 
     //region PublicMethods

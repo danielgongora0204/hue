fce0fe22e6469af09a3901fe6f1246a4d09acaaf
danielgongora0204 <43617580+danielgongora0204@users.noreply.github.com>
2022-06-06 20:20:02 -0700

    Update ObservableViewModel.kt
    Refactor code

--- app/src/main/java/com/gig/hue/utilities/ObservableViewModel.kt
+++ app/src/main/java/com/gig/hue/utilities/ObservableViewModel.kt
@@ -1,6 +1,5 @@
 package com.gig.hue.utilities
 
-import androidx.databinding.Bindable
 import androidx.databinding.Observable
 import androidx.databinding.PropertyChangeRegistry
 import androidx.lifecycle.ViewModel
@@ -16,21 +15,11 @@ open class ObservableViewModel : ViewModel(), Observable {
         callbacks.remove(callback)
     }
 
-    /**
-     * Notifies listeners that all properties of this instance have changed.
-     */
     @Suppress("unused")
     fun notifyChange() {
         callbacks.notifyCallbacks(this, 0, null)
     }
 
-    /**
-     * Notifies listeners that a specific property has changed. The getter for the property
-     * that changes should be marked with [Bindable] to generate a field in
-     * `BR` to be used as `fieldId`.
-     *
-     * @param fieldId The generated BR id for the Bindable field.
-     */
     fun notifyPropertyChanged(fieldId: Int) {
         callbacks.notifyCallbacks(this, fieldId, null)
     }

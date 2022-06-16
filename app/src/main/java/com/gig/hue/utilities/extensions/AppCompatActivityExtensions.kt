e432619d258b58254ac1b8363b63561dd77b1d7c
danielgongora0204 <43617580+danielgongora0204@users.noreply.github.com>
2022-06-16 13:09:49 -0700

    Extensions

---
+++ app/src/main/java/com/gig/hue/utilities/extensions/AppCompatActivityExtensions.kt
@@ -0,0 +1,24 @@
+package com.gig.hue.com.gig.hue.utilities.extensions
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+fun <T> AppCompatActivity.collectLatestLifecycleFlow(flow: StateFlow<T>, collect: suspend (T) -> Unit) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collectLatest(collect)
+        }
+    }
+}
+
+fun <T> AppCompatActivity.collectFlow(flow: SharedFlow<T>, collect: suspend (T) -> Unit) {
+    lifecycleScope.launch {
+        flow.collectLatest(collect)
+    }
+}
